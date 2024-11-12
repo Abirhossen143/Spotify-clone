98248eadb5869a4e4bd7d36cf22cd08e3cadcde5
Abir Hossen <abirhossen1050@gmail.com>
2024-11-12 12:23:17 +0600

    sidebar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Sidebar from "./component/Sidebar";
+
+function App() {
+  return (
+    <div className="h-screen bg-black">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;

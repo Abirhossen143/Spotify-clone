4d0530d07dfe5f0109b44a138edbd2682343ccdb
Abir Hossen <abirhossen1050@gmail.com>
2024-11-12 23:20:00 +0600

    reactrouterdom install

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Player from "./component/Player";
 import Sidebar from "./component/Sidebar";
 
 function App() {
   return (
     <div className="h-screen bg-black">
-      <Sidebar />
+      <div className="h-[90%] flex">
+        <Sidebar />
+      </div>
+      <Player />
     </div>
   );
 }
